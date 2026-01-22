996f090b747b55fcd91aa42aa82d856eb2ff2196
ramesh486 <rameshkrishnan@ebizoncloud.com>
2026-01-22 18:17:51 +0530

    updated

--- terraform/qco-ua1-08-budgets-stack/provider.tf
+++ terraform/qco-ua1-08-budgets-stack/provider.tf
@@ -3,6 +3,6 @@ provider "oci" {
   user_ocid		   = var.user_ocid
   tenancy_ocid     = var.tenancy_ocid
   fingerprint      = var.fingerprint
-  private_key	   = var.api_private_key
+  private_key	   = var.private_key
   ignore_defined_tags = ["Oracle-Tags.CreatedBy", "Oracle-Tags.CreatedOn"]
 }
