0a9700316123aa448ce9192a6ac236d12bbbf7a0
ramesh486 <rameshkrishnan@ebizoncloud.com>
2026-02-12 10:03:47 +0530

    oem

--- Networking/qco-ua1-05-prod-nonjde-oem-vcn-stack/locals_common.tf
+++ Networking/qco-ua1-05-prod-nonjde-oem-vcn-stack/locals_common.tf
@@ -2,7 +2,7 @@ locals {
     common_route_rules_for_all_subnets = [
         {
             destination       = local.cidr_blocks.all_iad_services_in_oracle_services_network
-            network_entity_id = module.vcn_prod_nonjde_servicegateway_private.service_gateway_ocid
+            network_entity_id = module.vcn_nonjde_servicegateway_private.service_gateway_ocid
             destination_type  = "SERVICE_CIDR_BLOCK"
         },
     ]
